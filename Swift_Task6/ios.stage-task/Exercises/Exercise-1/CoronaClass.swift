76def2e0a122523f2ee294c6f14214f8afdb9e23
Vladimir Benko <vnbenko@gmail.com>
2021-06-21 18:22:58 +0300

    feat: implement logic for exercise 1

--- Swift_Task6/ios.stage-task/Exercises/Exercise-1/CoronaClass.swift
+++ Swift_Task6/ios.stage-task/Exercises/Exercise-1/CoronaClass.swift
@@ -1,18 +1,48 @@
 import Foundation
 
 class CoronaClass {
- 
-     var seats = [Int]()
+    
+    private var seatsArray = [Int]()
+    private var seatsCount: Int
+    
+    var seats: [Int] {
+        get {
+            self.seatsArray.sorted()
+        }
+        set {
+            self.seatsArray = newValue
+        }
+    }
 
-     init(n: Int) {
- 
-     }
-     
-     func seat() -> Int {
-        -1
-     }
-     
-     func leave(_ p: Int) {
-         
-     }
+    init(n: Int) {
+        self.seatsCount = n
+    }
+    
+    func seat() -> Int {
+        var seat = 0
+        var distance = 0
+        
+        if self.seats.count < self.seatsCount && !self.seats.isEmpty {
+            if self.seats.first == 0 && self.seats.count == 1 || self.seats.last != self.seatsCount - 1 {
+                seat = self.seatsCount - 1
+            }
+            else {
+                for index in 1..<self.seats.count {
+                    if (self.seats[index] - self.seats[index - 1]) / 2 > distance {
+                        distance = (self.seats[index] - self.seats[index - 1]) / 2
+                        seat = self.seats[index - 1] + distance
+                    }
+                }
+            }
+        }
+        self.seats.append(seat)
+        print(seat)
+        return seat
+    }
+    
+    func leave(_ p: Int) {
+        if let index = self.seats.firstIndex(of: p) {
+            self.seats.remove(at: index)
+        }
+    }
 }
